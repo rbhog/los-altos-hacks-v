88302b32f25494af60343300d5cbad47f93c2707
Jason Liu <shadowbox826@gmail.com>
2021-04-24 18:43:31 -0400

    add da landing

--- frontend/los-altos/src/App.js
+++ frontend/los-altos/src/App.js
@@ -14,26 +14,17 @@ import { ColorModeSwitcher } from './ColorModeSwitcher';
 import Map from './components/Map/Map';
 import Drawer from './components/Drawer/Drawer';
 import Sidebar from './components/Sidebar/Sidebar';
+import Landing from './components/Landing/Landing'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+        <Grid minH="100vh">
+          {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
           <VStack spacing={8}>
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+
+            <Landing/>
             <Map />
             <Drawer />
             <Sidebar />
---
+++ frontend/los-altos/src/components/Landing/Landing.jsx
@@ -0,0 +1,14 @@
+import { Slide, Box, useDisclosure, Button } from '@chakra-ui/react';
+import './landing-style.css';
+const Landing = () => {
+
+  return (
+    <>
+      <Box w="100%" h="1080px" bg="grey">
+            hello!
+      </Box>
+    </>
+  );
+};
+
+export default Landing;
