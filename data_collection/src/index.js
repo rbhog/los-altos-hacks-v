cdea2c2b81db9bf4bd71dd963e5d9785723bd4c8
Rithvik <rbhogavilli@gmail.com>
2021-04-24 14:12:52 -0400

    Add average income to neighborhood data

--- data_collection/src/index.js
+++ data_collection/src/index.js
@@ -12,12 +12,15 @@ const base_neighborhoods = "https://opendata.arcgis.com/datasets/de63a68eb767454
 
 const outputPath = path.join(__dirname, "../data")
 
+let average = (array) => array.reduce((a, b) => a + b) / array.length;
+
 fs.ensureDirSync(outputPath)
 
 var codes = []
 
 var master = {
-    "points": []
+    type: "FeatureCollection",
+    features: []
 }
 
 var casesMaster = {
@@ -51,34 +54,19 @@ var census = {}
 //     fs.writeFileSync(path.join(outputPath, "total_cases.json"), JSON.stringify(casesMaster))
 // })
         
-// axios.get(neighborhood_tract).then((res) => {
-//         fs.writeFileSync(path.join(outputPath, "neighborhoods_to_censustracts.geojson"), JSON.stringify(res.data))
-// })
-
-// //feature.properties["DC_HPN_NAME"]
-            
-// axios.get("https://opendata.arcgis.com/datasets/de63a68eb7674548ae0ac01867123f7e_13.geojson").then((res) => {
-// fs.writeFileSync(path.join(outputPath, "health_neighborhoods.geojson"), JSON.stringify(res.data))
-// })
-
 
 // get tracts by each neighborhood
 axios.get(neighborhood_tract).then((res) => {
     tracts = res.data
-})
-
-
-axios.get(median_income_by_tract).then((res) => { // gets median income
+    return axios.get(median_income_by_tract)
+}).then((res) => {
     console.log(res.data.features[0].properties)
     //FAGI_MEDIAN_2015
     census = res.data
-    //fs.writeFileSync(path.join(outputPath, "census2010.geojson"), JSON.stringify(res.data))
-})
-
-
-axios.get(base_neighborhoods).then((res) => {
+    fs.writeFileSync(path.join(outputPath, "census2010.geojson"), JSON.stringify(res.data))
+    return axios.get(base_neighborhoods)
+}).then((res) => {
     res.data.features.forEach(feature => {
-        var toWrite = {}
         var tractsInArea = []
         //get census tracts for given feature
         tracts.features.forEach(tract => {
@@ -87,23 +75,36 @@ axios.get(base_neighborhoods).then((res) => {
             }
         })
 
-        var incomes = []
-
-        tractsInArea.forEach(geoid => {
-
+        var valid = []
+        tractsInArea.forEach(geoid => { // get the census data for each of the tracts
+            valid = valid.concat(census.features.filter(census_feature => census_feature.properties["GEOID"] === geoid))
         })
 
-        console.log(tractsInArea)
+        var medianIncomes = valid.map(
+            function(element) { return element.properties["FAGI_MEDIAN_2015"] }
+        )
 
-        // master.push()
+        var averageIncome = 0
+
+        if (medianIncomes.length > 0) {
+            averageIncome = average(medianIncomes)
+        }
+
+        feature.properties["AVERAGE_INCOME"] = averageIncome
+        console.log(`Tracts in neighborhood ${tractsInArea.length}`)
+        console.log(valid.length)
+        console.log(medianIncomes)
+        console.log(averageIncome)
+        
+        master.features.push(feature)
     })
+
+    fs.writeFileSync(path.join(outputPath, "health_neighborhoods1.geojson"), JSON.stringify(master))
 })
-
-
 
 // get a health neighborhood from the geojson - DONE
 // get the census tracts for given neighborhood using neighborhoods_to_censustracts.geojson - DONE
-// average the FAGI_MEDIAN_2015 for all census tracts in a given neighborhood
+// average the FAGI_MEDIAN_2015 for all census tracts in a given neighborhood - DONE
 // add the median income property to the neighborhood feature
 // push the feature to the master collection
 // write the file
